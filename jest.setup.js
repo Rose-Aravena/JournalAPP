f9833afabc555aa115716267c6bda1f7497bc920
Rose Aravena Madariaga <rosearavenamadariaga@MacBook-Air-de-Rose.local>
2022-10-14 17:28:12 -0300

    environment

--- jest.setup.js
+++ jest.setup.js
@@ -1,2 +1,11 @@
 import 'whatwg-fetch';
 import 'setimmediate';
+import { getEnvironments } from './src/helpers/getEnvironments';
+
+require('dotenv').config({
+    path: '.env.test'
+});
+
+jest.mock('./src/helpers/getEnvironments', () => ({
+    getEnvironments: () => ({...process.env})
+}));
--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,28 +1,27 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore/lite'
+import { getEnvironments } from "../helpers/getEnvironments";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// const firebaseConfig = {
-//     apiKey: "AIzaSyB5CeXa0rZjpo4bSWqgPLgEz9KJ47E3elk",
-//     authDomain: "journal-react-1476d.firebaseapp.com",
-//     projectId: "journal-react-1476d",
-//     storageBucket: "journal-react-1476d.appspot.com",
-//     messagingSenderId: "994759350224",
-//     appId: "1:994759350224:web:9a157560c85b412c6b8bd5"
-// };
+const {
+    VITE_APIKEY,
+    VITE_AUTHDOMAIN,
+    VITE_PROJECTID,
+    VITE_STORAGEBUCKET,
+    VITE_MESSAGINGSENDERID,
+    VITE_APPID
+} = getEnvironments();
 
-// testing
 const firebaseConfig = {
-    apiKey: "AIzaSyDgnS5goucP25NjFDlF19jCtPMFjiTJHOs",
-    authDomain: "testing-be4b6.firebaseapp.com",
-    projectId: "testing-be4b6",
-    storageBucket: "testing-be4b6.appspot.com",
-    messagingSenderId: "797730913293",
-    appId: "1:797730913293:web:5b2b5e8d0046603b9ca944"
+    apiKey: VITE_APIKEY,
+    authDomain: VITE_AUTHDOMAIN,
+    projectId: VITE_PROJECTID,
+    storageBucket: VITE_STORAGEBUCKET,
+    messagingSenderId: VITE_MESSAGINGSENDERID,
+    appId: VITE_APPID
 };
 
 // Initialize Firebase
